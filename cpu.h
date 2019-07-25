81e6df7c7bc9a04e2f6121eabf855a21ddcbbc51
Drew Schmidt <wrathematics@gmail.com>
2019-07-24 21:15:49 -0400

    add interface headers

---
+++ cpu.h
@@ -0,0 +1,12 @@
+#ifndef FML_CPU_H
+#define FML_CPU_H
+
+
+#include "src/errors.h"
+#include "src/rand.h"
+
+#include "src/cpu/mat.h"
+#include "src/cpu/types.h"
+
+
+#endif
---
+++ mpi.h
@@ -0,0 +1,15 @@
+#ifndef FML_MPI_H
+#define FML_MPI_H
+
+
+#include "src/errors.h"
+#include "src/rand.h"
+
+#include "src/mpi/dmat.h"
+#include "src/mpi/errors.h"
+#include "src/mpi/grid.h"
+#include "src/mpi/types.h"
+#include "src/mpi/utils.h"
+
+
+#endif
