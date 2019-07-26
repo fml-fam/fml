8b19625f3199ad6f5e889ed5c42f22d47e5b1810
Drew Schmidt <wrathematics@gmail.com>
2019-07-26 17:36:22 -0400

    add base matrix class

---
+++ src/matrix.hh
@@ -0,0 +1,25 @@
+#ifndef FML_MATRIX_H
+#define FML_MATRIX_H
+
+
+#include <cstdint>
+
+typedef int len_t;
+
+
+template <typename REAL>
+class matrix
+{
+  public:
+    len_t nrows() const {return m;};
+    len_t ncols() const {return n;};
+    REAL* data_ptr() {return data;};
+  
+  protected:
+    len_t m;
+    len_t n;
+    REAL *data;
+};
+
+
+#endif
