6cb35af04755e2a9883acc70dc2d2d765a72d6d7
Drew Schmidt <wrathematics@gmail.com>
2019-07-26 17:36:31 -0400

    add cpu matrix class

---
+++ src/cpu/cpumat.hh
@@ -0,0 +1,103 @@
+#ifndef FML_CPUMAT_H
+#define FML_CPUMAT_H
+
+
+#include <cstdlib>
+#include <cstring>
+#include <random>
+
+#include "../matrix.hh"
+
+template <typename REAL>
+class cpumat : public matrix<REAL>
+{
+  public:
+    cpumat(len_t nrows, len_t ncols);
+    cpumat(REAL *data, len_t nrows, len_t ncols);
+    ~cpumat();
+    
+    void print(uint8_t ndigits=4);
+    
+    void fill_zero();
+    void fill_eye();
+    void fill_runif(int seed, REAL min=0, REAL max=1);
+};
+
+
+
+template <typename REAL>
+cpumat<REAL>::cpumat(len_t nrows, len_t ncols)
+{
+  size_t len = nrows * ncols * sizeof(REAL);
+  this->data = (REAL*) malloc(len);
+  if (this->data == NULL)
+    throw std::bad_alloc();
+  
+  this->m = nrows;
+  this->n = ncols;
+}
+
+template <typename REAL>
+cpumat<REAL>::cpumat(REAL *data_, len_t nrows, len_t ncols)
+{
+  this->m = nrows;
+  this->n = ncols;
+  this->data = data_;
+}
+
+template <typename REAL>
+cpumat<REAL>::~cpumat()
+{
+  if (this->data)
+    free(this->data);
+}
+
+
+
+template <typename REAL>
+void cpumat<REAL>::print(uint8_t ndigits)
+{
+  for (int i=0; i<this->m; i++)
+  {
+    for (int j=0; j<this->n; j++)
+      printf("%.*f ", ndigits, this->data[i + this->m*j]);
+    
+    putchar('\n');
+  }
+  
+  putchar('\n');
+}
+
+
+
+template <typename REAL>
+void cpumat<REAL>::fill_zero()
+{
+  size_t len = (this->m) * (this->n) * sizeof(REAL);
+  memset(this->data, 0, len);
+}
+
+template <typename REAL>
+void cpumat<REAL>::fill_eye()
+{
+  this->fill_zero();
+  for (len_t i=0; i<this->m && i<this->n; i++)
+    this->data[i + this->m*i] = 1;
+}
+
+template <typename REAL>
+void cpumat<REAL>::fill_runif(int seed, REAL min, REAL max)
+{
+  std::mt19937 mt(seed);
+  for (len_t j=0; j<this->n; j++)
+  {
+    for (len_t i=0; i<this->m; i++)
+    {
+      static std::uniform_real_distribution<REAL> dist(min, max);
+      this->data[i + this->m*j] = dist(mt);
+    }
+  }
+}
+
+
+#endif
