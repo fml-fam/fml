904d616f69d7918ef8aa5f926ced743126a056f1
Drew Schmidt <wrathematics@gmail.com>
2019-07-27 12:57:16 -0400

    add more blacs prototypes

--- src/mpi/_blacs_prototypes.h
+++ src/mpi/_blacs_prototypes.h
@@ -2,11 +2,22 @@
 #define FML_MPI__BLACS_PROTOTYPES_H
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern void Cblacs_exit(int NotDone);
 extern void Cblacs_get(int ictxt, int what, int *val);
 extern void Cblacs_gridexit(int Contxt);
 extern void Cblacs_gridinfo(int ConTxt, int *nprow, int *npcol, int *myrow, int *mycol);
 extern void Cblacs_gridinit(int *ConTxt, char *order, int nprow, int npcol);
+extern void Cblacs_barrier(int ictxt, char *scope);
+extern void Cblacs_pinfo(int *mypnum, int *nprocs);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif
