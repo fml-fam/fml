07e5837922c05768db94f058674c0632f128b9c6
Drew Schmidt <wrathematics@gmail.com>
2019-07-28 20:58:14 -0400

    add basic cpu example

---
+++ examples/cpu/cpumat.cpp
@@ -0,0 +1,21 @@
+#include <cpu/cpumat.hh>
+
+int main()
+{
+  cpumat<float> x = cpumat<float>(5, 5);
+  len_t m = x.nrows();
+  len_t n = x.ncols();
+  printf("%dx%d\n", m, n);
+  
+  x.fill_eye();
+  x.print();
+  
+  float *data = x.data_ptr();
+  data[0] = 3;
+  x.print();
+  
+  x.fill_runif(1234);
+  x.print();
+  
+  return 0;
+}
