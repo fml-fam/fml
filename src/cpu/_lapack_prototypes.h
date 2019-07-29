47f7e226d11539402dce3d94d030baa068e4623a
Drew Schmidt <wrathematics@gmail.com>
2019-07-29 18:48:47 -0400

    add gemm prototypes and wrappers

--- src/cpu/_lapack_prototypes.h
+++ src/cpu/_lapack_prototypes.h
@@ -2,9 +2,27 @@
 #define FML_CPU__LAPACK_PROTOTYPES_H
 
 
+#ifndef restrict
+#define restrict __restrict__
+#endif
+
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+
+extern void sgemm_(const char *transa, const char *transb, const int *m,
+  const int *n, const int *k, const float *restrict alpha,
+  const float *restrict a, const int *lda, const float *restrict b,
+  const int *ldb, const float *beta, float *restrict c, const int *ldc);
+
+extern void dgemm_(const char *transa, const char *transb, const int *m,
+  const int *n, const int *k, const double *restrict alpha,
+  const double *restrict a, const int *lda, const double *restrict b,
+  const int *ldb, const double *beta, double *restrict c, const int *ldc);
+
 
 
 extern void sgetrf_(const int *const restrict m, const int *const restrict n,
--- src/cpu/lapack.hh
+++ src/cpu/lapack.hh
@@ -7,14 +7,32 @@
 
 namespace lapack
 {
-  void getrf(const int m, const int n, float *a, int *ipiv, int *info)
+  void gemm(const char transa, const char transb, const int m,
+    const int n, const int k, const float alpha,
+    const float *restrict a, const int lda, const float *restrict b,
+    const int ldb, const float beta, float *restrict c, const int ldc)
   {
-    sgetrf_(&m, &n, a, &m, ipiv, info);
+    sgemm_(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
   }
   
-  void getrf(const int m, const int n, double *a, int *ipiv, int *info)
+  void gemm(const char transa, const char transb, const int m,
+    const int n, const int k, const double alpha,
+    const double *restrict a, const int lda, const double *restrict b,
+    const int ldb, const double beta, double *restrict c, const int ldc)
   {
-    dgetrf_(&m, &n, a, &m, ipiv, info);
+    dgemm_(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
+  }
+  
+  
+  
+  void getrf(const int m, const int n, float *a, const int lda, int *ipiv, int *info)
+  {
+    sgetrf_(&m, &n, a, &lda, ipiv, info);
+  }
+  
+  void getrf(const int m, const int n, double *a, const int lda, int *ipiv, int *info)
+  {
+    dgetrf_(&m, &n, a, &lda, ipiv, info);
   }
 }
 
