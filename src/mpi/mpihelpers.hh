236655cb4957a5ade1397517d1fa47ce62fe1e50
Drew Schmidt <wrathematics@gmail.com>
2019-07-28 20:55:15 -0400

    minor

--- src/mpi/mpihelpers.hh
+++ src/mpi/mpihelpers.hh
@@ -19,9 +19,6 @@ namespace mpihelpers
     len_local_t m_local = mpi.nrows_local();
     len_local_t n_local = mpi.ncols_local();
     
-    int mb = mpi.bf_rows();
-    int nb = mpi.bf_cols();
-    
     grid g = mpi.get_grid();
     
     cpumat<REAL> cpu(m, n);
@@ -36,8 +33,9 @@ namespace mpihelpers
       {
         for (len_local_t i=0; i<n_local; i++)
         {
-          int gi = bcutils::l2g(i, mb, g.nprow(), g.myrow());
-          int gj = bcutils::l2g(j, nb, g.npcol(), g.mycol());
+          int gi = bcutils::l2g(i, mpi.bf_rows(), g.nprow(), g.myrow());
+          int gj = bcutils::l2g(j, mpi.bf_cols(), g.npcol(), g.mycol());
+          
           gbl[gi + m*gj] = sub[i + m_local*j];
         }
       }
