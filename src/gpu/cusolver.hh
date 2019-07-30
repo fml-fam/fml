43272147c0b16d35d3f3458003165052ef3d2889
Drew Schmidt <wrathematics@gmail.com>
2019-07-30 18:40:26 -0400

    add gemm bindings

--- src/gpu/cusolver.hh
+++ src/gpu/cusolver.hh
@@ -7,6 +7,18 @@
 
 namespace lapack
 {
+  cublasStatus_t cu_gemm(cublasHandle_t handle, cublasOperation_t transa, cublasOperation_t transb, int m, int n, int k, const float *alpha, const float *A, int lda, const float *B, int ldb, const float *beta, float *C, int ldc)
+  {
+    cublasSgemm(handle, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+  }
+  
+  cublasStatus_t cu_gemm(cublasHandle_t handle, cublasOperation_t transa, cublasOperation_t transb, int m, int n, int k, const double *alpha, const double *A, int lda, const double *B, int ldb, const double *beta, double *C, int ldc)
+  {
+    cublasSgemm(handle, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+  }
+  
+  
+  
   cusolverStatus_t cu_getrf_buflen(cusolverDnHandle_t handle, int m, int n, float *A, int lda, int *lwork)
   {
     return cusolverDnSgetrf_bufferSize(handle, m, n, A, lda, lwork);
