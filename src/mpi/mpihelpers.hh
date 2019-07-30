4c1f4c90f9c502b465f16a5d08597a3b5b26b63a
Drew Schmidt <wrathematics@gmail.com>
2019-07-29 20:14:32 -0400

    add cpu2mpi

--- src/mpi/mpihelpers.hh
+++ src/mpi/mpihelpers.hh
@@ -56,6 +56,40 @@ namespace mpihelpers
     
     return cpu;
   }
+  
+  
+  
+  template <typename REAL>
+  mpimat<REAL> cpu2mpi(cpumat<REAL> &cpu, grid g, int bf_rows=16, int bf_cols=16)
+  {
+    len_t m = cpu.nrows();
+    len_t n = cpu.ncols();
+    
+    mpimat<REAL> mpi(g, m, n, bf_rows, bf_cols);
+    mpi.fill_zero();
+    
+    len_local_t m_local = mpi.nrows_local();
+    len_local_t n_local = mpi.ncols_local();
+    
+    REAL *gbl = cpu.data_ptr();
+    REAL *sub = mpi.data_ptr();
+    
+    if (m_local > 0 && n_local > 0)
+    {
+      for (len_local_t j=0; j<n_local; j++)
+      {
+        for (len_local_t i=0; i<m_local; i++)
+        {
+          int gi = bcutils::l2g(i, mpi.bf_rows(), g.nprow(), g.myrow());
+          int gj = bcutils::l2g(j, mpi.bf_cols(), g.npcol(), g.mycol());
+          
+          sub[i + m_local*j] = gbl[gi + m*gj];
+        }
+      }
+    }
+    
+    return mpi;
+  }
 }
 
 
