6735d2e52c9e546771de728685d746c3c4bd895f
Drew Schmidt <wrathematics@gmail.com>
2019-08-14 21:24:36 -0400

    mark pxgemm prototypes extern

--- src/mpi/_scalapack_prototypes.h
+++ src/mpi/_scalapack_prototypes.h
@@ -7,13 +7,13 @@ extern "C" {
 #endif
 
 
-void psgemm_(const char *transa, const char *transb, const int *m, const int *n,
+extern void psgemm_(const char *transa, const char *transb, const int *m, const int *n,
   const int *k, const float *alpha, const float *a, const int *ia,
   const int *ja, const int *desca, const float *b, const int *ib,
   const int *jb, const int *descb, const float *beta, float *c, const int *ic,
   const int *jc, const int *descc);
 
-void pdgemm_(const char *transa, const char *transb, const int *m, const int *n,
+extern void pdgemm_(const char *transa, const char *transb, const int *m, const int *n,
   const int *k, const double *alpha, const double *a, const int *ia,
   const int *ja, const int *desca, const double *b, const int *ib,
   const int *jb, const int *descb, const double *beta, double *c, const int *ic,
