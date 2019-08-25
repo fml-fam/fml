6b0b440a657e37f841dc71170f48ec350402fbb6
Drew Schmidt <wrathematics@gmail.com>
2019-08-25 18:39:49 -0400

    add fill_eye gpu kernel

---
+++ src/gpu/kernelfuns.hh
@@ -0,0 +1,29 @@
+#ifndef FML_GPU_KERNELFUNS_H
+#define FML_GPU_KERNELFUNS_H
+
+
+#include <cuda_runtime.h>
+
+#include "../matrix.hh"
+
+
+namespace kernelfuns
+{
+  template <typename REAL>
+  __global__ void kernel_fill_eye(len_t m, len_t n, REAL *data)
+  {
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    int j = blockDim.y*blockIdx.y + threadIdx.y;
+    
+    if (i < m && j < n)
+    {
+      if (i == j)
+        data[i + m*j] = (REAL) 1;
+      else
+        data[i + m*j] = (REAL) 0;
+    }
+  }
+}
+
+
+#endif
