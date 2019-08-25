0f53c7f5e5fee6bbc72c3e0d1f93001ee972156d
Drew Schmidt <wrathematics@gmail.com>
2019-08-25 18:40:50 -0400

    Madd fill_linspace gpu kernel

--- src/gpu/kernelfuns.hh
+++ src/gpu/kernelfuns.hh
@@ -39,6 +39,22 @@ namespace kernelfuns
   
   
   template <typename REAL>
+  __global__ void kernel_fill_linspace(const REAL min, const REAL max, len_t m, len_t n, REAL *data)
+  {
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    int j = blockDim.y*blockIdx.y + threadIdx.y;
+    
+    if (i < m && j < n)
+    {
+      REAL v = (max-min)/((REAL) m*n-1);
+      len_t ind = i + m*j;
+      data[ind] = v*((REAL) ind) + min;
+    }
+  }
+  
+  
+  
+  template <typename REAL>
   __global__ void kernel_scale(const REAL s, len_t m, len_t n, REAL *data)
   {
     int i = blockDim.x*blockIdx.x + threadIdx.x;
