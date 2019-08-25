db04c0e7c85739ab809db7317cde9ec70af2fba5
Drew Schmidt <wrathematics@gmail.com>
2019-08-25 18:40:08 -0400

    add fill_val gpu kernel

--- src/gpu/kernelfuns.hh
+++ src/gpu/kernelfuns.hh
@@ -23,6 +23,18 @@ namespace kernelfuns
         data[i + m*j] = (REAL) 0;
     }
   }
+  
+  
+  
+  template <typename REAL>
+  __global__ void kernel_fill_val(const REAL v, len_t m, len_t n, REAL *data)
+  {
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    int j = blockDim.y*blockIdx.y + threadIdx.y;
+    
+    if (i < m && j < n)
+      data[i + m*j] = v;
+  }
 }
 
 
