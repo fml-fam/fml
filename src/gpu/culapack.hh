3b6299eeec3a7e46d076a628f669928f69926753
Drew Schmidt <wrathematics@gmail.com>
2019-08-26 19:46:00 -0400

    add geam wrapper for gpumat

--- src/gpu/culapack.hh
+++ src/gpu/culapack.hh
@@ -31,6 +31,18 @@ namespace culapack
   
   
   
+  inline cublasStatus_t geam(cublasHandle_t handle, cublasOperation_t transa, cublasOperation_t transb, int m, int n, const float *alpha, const float *A, int lda, const float *beta, const float *B, int ldb, float *C, int ldc)
+  {
+    return cublasSgeam(handle, transa, transb, m, n, alpha, A, lda, beta, B, ldb, C, ldc);
+  }
+  
+  inline cublasStatus_t geam(cublasHandle_t handle, cublasOperation_t transa, cublasOperation_t transb, int m, int n, const double *alpha, const double *A, int lda, const double *beta, const double *B, int ldb, double *C, int ldc)
+  {
+    return cublasSgeam(handle, transa, transb, m, n, alpha, A, lda, beta, B, ldb, C, ldc);
+  }
+  
+  
+  
   inline cusolverStatus_t getrf_buflen(cusolverDnHandle_t handle, int m, int n, float *A, int lda, int *lwork)
   {
     return cusolverDnSgetrf_bufferSize(handle, m, n, A, lda, lwork);
