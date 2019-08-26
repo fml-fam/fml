ee230dd1a887867aae38918510a4b8666ec09587
wrathematics <wrathematics@gmail.com>
2019-08-26 19:30:05 -0400

    use () accessors in cpumat tests

--- tests/cpu/linalg.cpp
+++ tests/cpu/linalg.cpp
@@ -17,27 +17,29 @@ TEMPLATE_TEST_CASE("matrix multiplication", "[linalg]", float, double)
   y.fill_linspace((float) n*n, 1.f);
   
   cpumat<TestType> z = linalg::matmult(false, false, (TestType)1, x, y);
-  const TestType *data = z.data_ptr();
-  REQUIRE( fltcmp::eq(data[0], 13) );
-  REQUIRE( fltcmp::eq(data[1], 20) );
-  REQUIRE( fltcmp::eq(data[2], 5) );
-  REQUIRE( fltcmp::eq(data[3], 8) );
+  REQUIRE( z.nrows() == n );
+  REQUIRE( z.ncols() == n );
+  
+  REQUIRE( fltcmp::eq(z(0), 13) );
+  REQUIRE( fltcmp::eq(z(1), 20) );
+  REQUIRE( fltcmp::eq(z(2), 5) );
+  REQUIRE( fltcmp::eq(z(3), 8) );
   
   linalg::matmult_noalloc(true, false, (TestType)1, x, y, z);
-  REQUIRE( fltcmp::eq(data[0], 10) );
-  REQUIRE( fltcmp::eq(data[1], 24) );
-  REQUIRE( fltcmp::eq(data[2], 4) );
-  REQUIRE( fltcmp::eq(data[3], 10) );
+  REQUIRE( fltcmp::eq(z(0), 10) );
+  REQUIRE( fltcmp::eq(z(1), 24) );
+  REQUIRE( fltcmp::eq(z(2), 4) );
+  REQUIRE( fltcmp::eq(z(3), 10) );
   
   linalg::matmult_noalloc(false, true, (TestType)1, x, y, z);
-  REQUIRE( fltcmp::eq(data[0], 10) );
-  REQUIRE( fltcmp::eq(data[1], 16) );
-  REQUIRE( fltcmp::eq(data[2], 6) );
-  REQUIRE( fltcmp::eq(data[3], 10) );
+  REQUIRE( fltcmp::eq(z(0), 10) );
+  REQUIRE( fltcmp::eq(z(1), 16) );
+  REQUIRE( fltcmp::eq(z(2), 6) );
+  REQUIRE( fltcmp::eq(z(3), 10) );
   
   linalg::matmult_noalloc(true, true, (TestType)1, x, y, z);
-  REQUIRE( fltcmp::eq(data[0], 8) );
-  REQUIRE( fltcmp::eq(data[1], 20) );
-  REQUIRE( fltcmp::eq(data[2], 5) );
-  REQUIRE( fltcmp::eq(data[3], 13) );
+  REQUIRE( fltcmp::eq(z(0), 8) );
+  REQUIRE( fltcmp::eq(z(1), 20) );
+  REQUIRE( fltcmp::eq(z(2), 5) );
+  REQUIRE( fltcmp::eq(z(3), 13) );
 }
