72331de1a41f1fb0394956dc120ca14c51ff4a38
Drew Schmidt <wrathematics@gmail.com>
2019-08-28 18:32:45 -0400

    add svd mpi example

---
+++ examples/mpi/svd.cpp
@@ -0,0 +1,32 @@
+#include <mpi/mpimat.hh>
+#include <mpi/linalg.hh>
+
+
+int main()
+{
+  grid g = grid(PROC_GRID_SQUARE);
+  g.info();
+  
+  len_t m = 3;
+  len_t n = 2;
+  
+  mpimat<float> x(g, m, n, 1, 1);
+  x.fill_linspace(1.f, (float)m*n);
+  
+  x.info();
+  x.print(0);
+  
+  cpuvec<float> s;
+  linalg::svd(x, s);
+  
+  if (g.rank0())
+  {
+    s.info();
+    s.print();
+  }
+  
+  g.exit();
+  g.finalize();
+  
+  return 0;
+}
