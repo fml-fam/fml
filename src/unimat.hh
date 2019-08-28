08c83291cd41cf8a19de8ce60ca03a392e23e186
Drew Schmidt <wrathematics@gmail.com>
2019-08-28 18:34:06 -0400

    forgot to add unimat header

---
+++ src/unimat.hh
@@ -0,0 +1,24 @@
+#ifndef FML_MATRIX_H
+#define FML_MATRIX_H
+
+
+#include "types.hh"
+
+
+template <typename REAL>
+class unimat
+{
+  public:
+    len_t nrows() const {return m;};
+    len_t ncols() const {return n;};
+    REAL* data_ptr() {return data;};
+    REAL* data_ptr() const {return data;};
+  
+  protected:
+    len_t m;
+    len_t n;
+    REAL *data;
+};
+
+
+#endif
