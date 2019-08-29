8afc9b6e02010bbb1f42dfc6cf5a7958c97dbf81
Drew Schmidt <wrathematics@gmail.com>
2019-08-29 07:43:47 -0400

    add first sketch of fp16 example

---
+++ examples/gpu/half.cu
@@ -0,0 +1,22 @@
+#include <gpu/card.hh>
+#include <gpu/gpumat.hh>
+#include <gpu/linalg.hh>
+
+
+int main()
+{
+  auto c = std::make_shared<card>(0);
+  c->info();
+  
+  len_t n = 5;
+  gpumat<__half> x(c, n, n);
+  x.fill_zero();
+  
+  gpumat<__half> y(c, n, n);
+  y.fill_zero();
+  
+  gpumat<__half> z = linalg::matmult(false, false, (__half)1.f, x, y);
+  z.info();
+  
+  return 0;
+}
