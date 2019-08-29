62a0c3dc350d28b65d8d1dc59489b3ea8c83e983
Drew Schmidt <wrathematics@gmail.com>
2019-08-29 07:42:01 -0400

    add fp16 gemm for gpu

--- src/gpu/culapack.hh
+++ src/gpu/culapack.hh
@@ -7,6 +7,11 @@
 
 namespace culapack
 {
+  inline cublasStatus_t gemm(cublasHandle_t handle, cublasOperation_t transa, cublasOperation_t transb, int m, int n, int k, const __half alpha, const __half *A, int lda, const __half *B, int ldb, const __half beta, __half *C, int ldc)
+  {
+    return cublasHgemm(handle, transa, transb, m, n, k, &alpha, A, lda, B, ldb, &beta, C, ldc);
+  }
+  
   inline cublasStatus_t gemm(cublasHandle_t handle, cublasOperation_t transa, cublasOperation_t transb, int m, int n, int k, const float alpha, const float *A, int lda, const float *B, int ldb, const float beta, float *C, int ldc)
   {
     return cublasSgemm(handle, transa, transb, m, n, k, &alpha, A, lda, B, ldb, &beta, C, ldc);
