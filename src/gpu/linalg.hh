d326c1c1725c381948fa310ce88dc6ae0d9d1c64
Drew Schmidt <wrathematics@gmail.com>
2019-09-06 06:18:44 -0400

    add crossprod/tcrossprod for gpumat

--- src/gpu/linalg.hh
+++ src/gpu/linalg.hh
@@ -44,6 +44,71 @@ namespace linalg
     if (check != CUBLAS_STATUS_SUCCESS)
       throw std::runtime_error("asdf");
   }
+  
+  
+  
+  // lower triangle of t(x) %*% x
+  template <typename REAL>
+  void crossprod(const REAL alpha, const gpumat<REAL> &x, gpumat<REAL> &ret)
+  {
+    const len_t m = x.nrows();
+    const len_t n = x.ncols();
+    
+    if (n != ret.nrows() || n != ret.ncols())
+      throw std::runtime_error("non-conformable arguments");
+    
+    auto cbh = x.get_card()->cb_handle();
+    cublasOperation_t trans = CUBLAS_OP_T;
+    cublasFillMode_t uplo = CUBLAS_FILL_MODE_LOWER;
+    
+    culapack::syrk(cbh, uplo, trans, n, m, alpha, x.data_ptr(), m, (REAL)0.0, ret.data_ptr(), n);
+  }
+  
+  template <typename REAL>
+  gpumat<REAL> crossprod(const REAL alpha, const gpumat<REAL> &x)
+  {
+    const len_t n = x.ncols();
+    auto c = x.get_card();
+    
+    gpumat<REAL> ret(c, n, n);
+    ret.fill_zero();
+    
+    crossprod(alpha, x, ret);
+    
+    return ret;
+  }
+  
+  
+  
+  template <typename REAL>
+  void tcrossprod(const REAL alpha, const gpumat<REAL> &x, gpumat<REAL> &ret)
+  {
+    const len_t m = x.nrows();
+    const len_t n = x.ncols();
+    
+    if (m != ret.nrows() || m != ret.ncols())
+      throw std::runtime_error("non-conformable arguments");
+    
+    auto cbh = x.get_card()->cb_handle();
+    cublasOperation_t trans = CUBLAS_OP_N;
+    cublasFillMode_t uplo = CUBLAS_FILL_MODE_LOWER;
+    
+    culapack::syrk(cbh, uplo, trans, m, n, alpha, x.data_ptr(), m, (REAL)0.0, ret.data_ptr(), m);
+  }
+  
+  template <typename REAL>
+  gpumat<REAL> tcrossprod(const REAL alpha, const gpumat<REAL> &x)
+  {
+    const len_t m = x.nrows();
+    auto c = x.get_card();
+    
+    gpumat<REAL> ret(c, m, m);
+    ret.fill_zero();
+    
+    tcrossprod(alpha, x, ret);
+    
+    return ret;
+  }
 }
 
 
