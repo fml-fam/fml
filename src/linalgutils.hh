68cfd7a475a7268cf975444a429541b575bcda0f
Drew Schmidt <wrathematics@gmail.com>
2019-09-08 11:46:32 -0400

    add matmult dimension checks

--- src/linalgutils.hh
+++ src/linalgutils.hh
@@ -2,6 +2,8 @@
 #define FML_LINALGUTILS_H
 
 
+#include <stdexcept>
+
 #include "types.hh"
 
 
@@ -9,6 +11,27 @@ namespace linalgutils
 {
   inline void matmult_params(const bool transx, const bool transy, const len_t mx, const len_t nx, const len_t my, const len_t ny, len_t *m, len_t *n, len_t *k)
   {
+    if (!transx && !transy)
+    {
+      if (nx != my)
+        throw std::runtime_error("non-conformable arguments");
+    }
+    else if (transx && transy)
+    {
+      if (mx != ny)
+        throw std::runtime_error("non-conformable arguments");
+    }
+    else if (transx && !transy)
+    {
+      if (mx != my)
+        throw std::runtime_error("non-conformable arguments");
+    }
+    else if (!transx && transy)
+    {
+      if (nx != ny)
+        throw std::runtime_error("non-conformable arguments");
+    }
+    
     // m = # rows of op(x)
     // n = # cols of op(y)
     // k = # cols of op(x)
