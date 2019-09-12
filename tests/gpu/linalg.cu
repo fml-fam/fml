61d629b9e96ad6d41b789689bd5f54e197a2e03a
Drew Schmidt <wrathematics@gmail.com>
2019-09-12 18:49:13 -0400

    use paren operator in gpu tests

--- tests/gpu/linalg.cu
+++ tests/gpu/linalg.cu
@@ -18,33 +18,29 @@ TEMPLATE_TEST_CASE("matrix addition", "[linalg]", float, double)
   y.fill_linspace((TestType) n*n, 1.f);
   
   auto z = linalg::add(false, false, (TestType)1.f, (TestType)1.f, x, y);
-  auto z_cpu = gpuhelpers::gpu2cpu(z);
   
   TestType v = (TestType) n*n + 1;
-  REQUIRE( fltcmp::eq(z_cpu(0, 0), v) );
-  REQUIRE( fltcmp::eq(z_cpu(1, 0), v) );
-  REQUIRE( fltcmp::eq(z_cpu(0, 1), v) );
-  REQUIRE( fltcmp::eq(z_cpu(1, 1), v) );
+  REQUIRE( fltcmp::eq(z(0, 0), v) );
+  REQUIRE( fltcmp::eq(z(1, 0), v) );
+  REQUIRE( fltcmp::eq(z(0, 1), v) );
+  REQUIRE( fltcmp::eq(z(1, 1), v) );
   
   linalg::add(false, false, (TestType)1.f, (TestType)1.f, x, z, z);
-  gpuhelpers::gpu2cpu(z, z_cpu);
   
-  REQUIRE( fltcmp::eq(z_cpu(0, 0), v+1) );
-  REQUIRE( fltcmp::eq(z_cpu(1, 0), v+2) );
-  REQUIRE( fltcmp::eq(z_cpu(0, 1), v+3) );
-  REQUIRE( fltcmp::eq(z_cpu(1, 1), v+4) );
+  REQUIRE( fltcmp::eq(z(0, 0), v+1) );
+  REQUIRE( fltcmp::eq(z(1, 0), v+2) );
+  REQUIRE( fltcmp::eq(z(0, 1), v+3) );
+  REQUIRE( fltcmp::eq(z(1, 1), v+4) );
   
   linalg::add(false, true, (TestType)1.f, (TestType)1.f, x, y, z);
-  gpuhelpers::gpu2cpu(z, z_cpu);
   
-  REQUIRE( fltcmp::eq(z_cpu(1, 0), v-1) );
-  REQUIRE( fltcmp::eq(z_cpu(0, 1), v+1) );
+  REQUIRE( fltcmp::eq(z(1, 0), v-1) );
+  REQUIRE( fltcmp::eq(z(0, 1), v+1) );
   
   linalg::add(true, false, (TestType)1.f, (TestType)1.f, x, y, z);
-  gpuhelpers::gpu2cpu(z, z_cpu);
   
-  REQUIRE( fltcmp::eq(z_cpu(1, 0), v+1) );
-  REQUIRE( fltcmp::eq(z_cpu(0, 1), v-1) );
+  REQUIRE( fltcmp::eq(z(1, 0), v+1) );
+  REQUIRE( fltcmp::eq(z(0, 1), v-1) );
 }
 
 
@@ -62,33 +58,28 @@ TEMPLATE_TEST_CASE("matrix multiplication", "[linalg]", float, double)
   REQUIRE( z.nrows() == n );
   REQUIRE( z.ncols() == n );
   
-  cpumat<TestType> z_cpu = gpuhelpers::gpu2cpu(z);
-  
-  REQUIRE( fltcmp::eq(z_cpu(0), 13) );
-  REQUIRE( fltcmp::eq(z_cpu(1), 20) );
-  REQUIRE( fltcmp::eq(z_cpu(2), 5) );
-  REQUIRE( fltcmp::eq(z_cpu(3), 8) );
+  REQUIRE( fltcmp::eq(z(0), 13) );
+  REQUIRE( fltcmp::eq(z(1), 20) );
+  REQUIRE( fltcmp::eq(z(2), 5) );
+  REQUIRE( fltcmp::eq(z(3), 8) );
   
   linalg::matmult(true, false, (TestType)1, x, y, z);
-  gpuhelpers::gpu2cpu(z, z_cpu);
-  REQUIRE( fltcmp::eq(z_cpu(0), 10) );
-  REQUIRE( fltcmp::eq(z_cpu(1), 24) );
-  REQUIRE( fltcmp::eq(z_cpu(2), 4) );
-  REQUIRE( fltcmp::eq(z_cpu(3), 10) );
+  REQUIRE( fltcmp::eq(z(0), 10) );
+  REQUIRE( fltcmp::eq(z(1), 24) );
+  REQUIRE( fltcmp::eq(z(2), 4) );
+  REQUIRE( fltcmp::eq(z(3), 10) );
   
   linalg::matmult(false, true, (TestType)1, x, y, z);
-  gpuhelpers::gpu2cpu(z, z_cpu);
-  REQUIRE( fltcmp::eq(z_cpu(0), 10) );
-  REQUIRE( fltcmp::eq(z_cpu(1), 16) );
-  REQUIRE( fltcmp::eq(z_cpu(2), 6) );
-  REQUIRE( fltcmp::eq(z_cpu(3), 10) );
+  REQUIRE( fltcmp::eq(z(0), 10) );
+  REQUIRE( fltcmp::eq(z(1), 16) );
+  REQUIRE( fltcmp::eq(z(2), 6) );
+  REQUIRE( fltcmp::eq(z(3), 10) );
   
   linalg::matmult(true, true, (TestType)1, x, y, z);
-  gpuhelpers::gpu2cpu(z, z_cpu);
-  REQUIRE( fltcmp::eq(z_cpu(0), 8) );
-  REQUIRE( fltcmp::eq(z_cpu(1), 20) );
-  REQUIRE( fltcmp::eq(z_cpu(2), 5) );
-  REQUIRE( fltcmp::eq(z_cpu(3), 13) );
+  REQUIRE( fltcmp::eq(z(0), 8) );
+  REQUIRE( fltcmp::eq(z(1), 20) );
+  REQUIRE( fltcmp::eq(z(2), 5) );
+  REQUIRE( fltcmp::eq(z(3), 13) );
 }
 
 
@@ -106,42 +97,36 @@ TEMPLATE_TEST_CASE("crossprod and tcrossprod", "[linalg]", float, double)
   REQUIRE( x_cp.nrows() == x.ncols() );
   REQUIRE( x_cp.ncols() == x.ncols() );
   
-  cpumat<TestType> x_cp_cpu = gpuhelpers::gpu2cpu(x_cp);
-  
-  REQUIRE( fltcmp::eq(x_cp_cpu(0, 0), 14) );
-  REQUIRE( fltcmp::eq(x_cp_cpu(1, 0), 32) );
-  REQUIRE( fltcmp::eq(x_cp_cpu(1, 1), 77) );
+  REQUIRE( fltcmp::eq(x_cp(0, 0), 14) );
+  REQUIRE( fltcmp::eq(x_cp(1, 0), 32) );
+  REQUIRE( fltcmp::eq(x_cp(1, 1), 77) );
   
   gpumat<TestType> x_tcp = linalg::tcrossprod((TestType)1, x);
   REQUIRE( x_tcp.nrows() == x.nrows() );
   REQUIRE( x_tcp.ncols() == x.nrows() );
   
-  cpumat<TestType> x_tcp_cpu = gpuhelpers::gpu2cpu(x_tcp);
-  
-  REQUIRE( fltcmp::eq(x_tcp_cpu(0, 0), 17) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(1, 0), 22) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(1, 1), 29) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(2, 0), 27) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(2, 1), 36) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(2, 2), 45) );
+  REQUIRE( fltcmp::eq(x_tcp(0, 0), 17) );
+  REQUIRE( fltcmp::eq(x_tcp(1, 0), 22) );
+  REQUIRE( fltcmp::eq(x_tcp(1, 1), 29) );
+  REQUIRE( fltcmp::eq(x_tcp(2, 0), 27) );
+  REQUIRE( fltcmp::eq(x_tcp(2, 1), 36) );
+  REQUIRE( fltcmp::eq(x_tcp(2, 2), 45) );
   
   // noalloc api
   x.fill_linspace((TestType) m*n, 1.f);
   
   linalg::crossprod((TestType)1, x, x_cp);
-  gpuhelpers::gpu2cpu(x_cp, x_cp_cpu);
-  REQUIRE( fltcmp::eq(x_cp_cpu(0, 0), 77) );
-  REQUIRE( fltcmp::eq(x_cp_cpu(1, 0), 32) );
-  REQUIRE( fltcmp::eq(x_cp_cpu(1, 1), 14) );
+  REQUIRE( fltcmp::eq(x_cp(0, 0), 77) );
+  REQUIRE( fltcmp::eq(x_cp(1, 0), 32) );
+  REQUIRE( fltcmp::eq(x_cp(1, 1), 14) );
   
   linalg::tcrossprod((TestType)1, x, x_tcp);
-  gpuhelpers::gpu2cpu(x_tcp, x_tcp_cpu);
-  REQUIRE( fltcmp::eq(x_tcp_cpu(0, 0), 45) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(1, 0), 36) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(1, 1), 29) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(2, 0), 27) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(2, 1), 22) );
-  REQUIRE( fltcmp::eq(x_tcp_cpu(2, 2), 17) );
+  REQUIRE( fltcmp::eq(x_tcp(0, 0), 45) );
+  REQUIRE( fltcmp::eq(x_tcp(1, 0), 36) );
+  REQUIRE( fltcmp::eq(x_tcp(1, 1), 29) );
+  REQUIRE( fltcmp::eq(x_tcp(2, 0), 27) );
+  REQUIRE( fltcmp::eq(x_tcp(2, 1), 22) );
+  REQUIRE( fltcmp::eq(x_tcp(2, 2), 17) );
 }
 
 
@@ -158,12 +143,9 @@ TEMPLATE_TEST_CASE("xpose", "[linalg]", float, double)
   REQUIRE( tx.nrows() == x.ncols() );
   REQUIRE( tx.ncols() == x.nrows() );
   
-  cpumat<TestType> x_cpu = gpuhelpers::gpu2cpu(x);
-  cpumat<TestType> tx_cpu = gpuhelpers::gpu2cpu(tx);
-  
-  REQUIRE( fltcmp::eq(x_cpu(0, 0), tx_cpu(0, 0)) );
-  REQUIRE( fltcmp::eq(x_cpu(1, 0), tx_cpu(0, 1)) );
-  REQUIRE( fltcmp::eq(x_cpu(2, 1), tx_cpu(1, 2)) );
+  REQUIRE( fltcmp::eq(x(0, 0), tx(0, 0)) );
+  REQUIRE( fltcmp::eq(x(1, 0), tx(0, 1)) );
+  REQUIRE( fltcmp::eq(x(2, 1), tx(1, 2)) );
 }
 
 
@@ -176,12 +158,11 @@ TEMPLATE_TEST_CASE("lu", "[linalg]", float, double)
   x.fill_linspace(1.f, (TestType) n*n);
   
   linalg::lu(x);
-  cpumat<TestType> x_cpu = gpuhelpers::gpu2cpu(x);
   
-  REQUIRE( fltcmp::eq(x_cpu(0, 0), 2) );
-  REQUIRE( fltcmp::eq(x_cpu(0, 1), 4) );
-  REQUIRE( fltcmp::eq(x_cpu(1, 0), 0.5) );
-  REQUIRE( fltcmp::eq(x_cpu(1, 1), 1) );
+  REQUIRE( fltcmp::eq(x(0, 0), 2) );
+  REQUIRE( fltcmp::eq(x(0, 1), 4) );
+  REQUIRE( fltcmp::eq(x(1, 0), 0.5) );
+  REQUIRE( fltcmp::eq(x(1, 1), 1) );
 }
 
 
@@ -190,22 +171,20 @@ TEMPLATE_TEST_CASE("svd", "[linalg]", float, double)
 {
   len_t n = 2;
   
-  cpuvec<TestType> v(n);
-  v(0) = (TestType) 2;
-  v(1) = (TestType) 5;
+  cpuvec<TestType> v_cpu(n);
+  v_cpu(0) = (TestType) 2;
+  v_cpu(1) = (TestType) 5;
   
-  cpumat<TestType> x_cpu(n, n);
-  x_cpu.fill_diag(v);
+  gpuvec<TestType> v = gpuhelpers::cpu2gpu(c, v_cpu);
   
-  gpumat<TestType> x = gpuhelpers::cpu2gpu(c, x_cpu);
+  gpumat<TestType> x(c, n, n);
+  x.fill_diag(v);
   
   gpuvec<TestType> s;
   linalg::svd(x, s);
   
-  cpuvec<TestType> s_cpu = gpuhelpers::gpu2cpu(s);
-  
   v.rev();
-  REQUIRE( v == s_cpu );
+  REQUIRE( v == s );
 }
 
 
@@ -218,12 +197,11 @@ TEMPLATE_TEST_CASE("invert", "[linalg]", float, double)
   x.fill_linspace(1.f, (TestType) n*n);
   
   linalg::invert(x);
-  cpumat<TestType> x_cpu = gpuhelpers::gpu2cpu(x);
   
-  REQUIRE( fltcmp::eq(x_cpu(0, 0), -2) );
-  REQUIRE( fltcmp::eq(x_cpu(1, 0), 1) );
-  REQUIRE( fltcmp::eq(x_cpu(0, 1), 1.5) );
-  REQUIRE( fltcmp::eq(x_cpu(1, 1), -0.5) );
+  REQUIRE( fltcmp::eq(x(0, 0), -2) );
+  REQUIRE( fltcmp::eq(x(1, 0), 1) );
+  REQUIRE( fltcmp::eq(x(0, 1), 1.5) );
+  REQUIRE( fltcmp::eq(x(1, 1), -0.5) );
 }
 
 
