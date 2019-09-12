04c7b54e6fe6dd9be79298ca66ff5e9eb57bbcc3
Drew Schmidt <wrathematics@gmail.com>
2019-09-12 18:44:18 -0400

    add types for parmat

--- src/types.hh
+++ src/types.hh
@@ -2,8 +2,15 @@
 #define FML_TYPES_H
 
 
+#define __STDC_FORMAT_MACROS
+#include <cinttypes>
+#include <cstdint>
+
 typedef int len_t;
 typedef int len_local_t;
+typedef uint64_t len_global_t;
+
+#define MPI_LENGLOBAL_T MPI_UINT64_T
 
 
 #endif
