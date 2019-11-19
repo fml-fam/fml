d0ebf9eeecdc8f57b0a015c409195322c48b1b86
Drew Schmidt <wrathematics@gmail.com>
2019-11-18 19:40:42 -0500

    add some gpu launcher utilities

--- src/gpu/launcher.hh
+++ src/gpu/launcher.hh
@@ -19,4 +19,47 @@
 #endif
 
 
+
+namespace kernel_launcher
+{
+  namespace
+  {
+    static const int BLOCK_SIZE = 16;
+    
+    static inline int grid_len(len_t len)
+    {
+      return (len + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    }
+  }
+  
+  
+  
+  static inline dim3 dim_block1()
+  {
+    dim3 block(BLOCK_SIZE);
+    return block;
+  }
+  
+  static inline dim3 dim_block2()
+  {
+    dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+    return block;
+  }
+  
+  
+  
+  static inline dim3 dim_grid(len_t len)
+  {
+    dim3 grid(grid_len(len));
+    return grid;
+  }
+  
+  static inline dim3 dim_grid(len_t m, len_t n)
+  {
+    dim3 grid(grid_len(m), grid_len(n));
+    return grid;
+  }
+}
+
+
 #endif
