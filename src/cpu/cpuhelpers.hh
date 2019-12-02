45cef13f4a6c2cbb3aec1e53f9b6fc6c9a793de6
Drew Schmidt <wrathematics@gmail.com>
2019-12-01 21:43:57 -0500

    add cpu copier

---
+++ src/cpu/cpuhelpers.hh
@@ -0,0 +1,34 @@
+#ifndef FML_CPU_CPUHELPERS_H
+#define FML_CPU_CPUHELPERS_H
+
+
+#include <stdexcept>
+
+#include "../arraytools/src/arraytools.hpp"
+
+#include "../cpu/cpumat.hh"
+#include "../cpu/cpuvec.hh"
+
+
+namespace cpuhelpers
+{
+  template <typename REAL_IN, typename REAL_OUT>
+  void cpu2cpu(const cpuvec<REAL_IN> &cpu_in, cpuvec<REAL_OUT> &cpu_out)
+  {
+    cpu_out.resize(cpu_in.size());
+    
+    arraytools::copy(cpu_in.size(), cpu_in.data_ptr(), cpu_out.data_ptr());
+  }
+  
+  template <typename REAL_IN, typename REAL_OUT>
+  void cpu2cpu(const cpumat<REAL_IN> &cpu_in, cpumat<REAL_OUT> &cpu_out)
+  {
+    cpu_out.resize(cpu_in.nrows(), cpu_in.ncols());
+    
+    size_t len = (size_t) cpu_in.nrows() * cpu_in.ncols();
+    arraytools::copy(len, cpu_in.data_ptr(), cpu_out.data_ptr());
+  }
+}
+
+
+#endif
