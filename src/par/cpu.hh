23967deeeaefb73be00546d38ddedbb3c2143642
Drew Schmidt <wrathematics@gmail.com>
2019-12-02 10:49:20 -0500

    pragma once and copyright boilerplate in par

--- src/par/cpu.hh
+++ src/par/cpu.hh
@@ -1,5 +1,10 @@
+// This file is part of fml which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
 #ifndef FML_PAR_CPU_H
 #define FML_PAR_CPU_H
+#pragma once
 
 
 #include "../cpu/cpumat.hh"
