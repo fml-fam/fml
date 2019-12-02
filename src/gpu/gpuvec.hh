1a52b95996440913124843610b59f5f25b43ee24
Drew Schmidt <wrathematics@gmail.com>
2019-12-02 10:47:12 -0500

    add copyright boilerplate

--- src/gpu/gpuvec.hh
+++ src/gpu/gpuvec.hh
@@ -1,3 +1,7 @@
+// This file is part of fml which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
 #ifndef FML_GPU_GPUVEC_H
 #define FML_GPU_GPUVEC_H
 #pragma once
--- src/mpi/mpimat.hh
+++ src/mpi/mpimat.hh
@@ -1,3 +1,7 @@
+// This file is part of fml which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
 #ifndef FML_MPI_MPIMAT_H
 #define FML_MPI_MPIMAT_H
 #pragma once
