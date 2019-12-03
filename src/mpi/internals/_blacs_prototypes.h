90fa75eba6d282ffc38c0261a92d79ba01bea580
Drew Schmidt <wrathematics@gmail.com>
2019-12-03 12:33:41 -0500

    mpi header guards

--- src/mpi/internals/_blacs_prototypes.h
+++ src/mpi/internals/_blacs_prototypes.h
@@ -2,8 +2,8 @@
 // License, Version 1.0. See accompanying file LICENSE or copy at
 // https://www.boost.org/LICENSE_1_0.txt
 
-#ifndef FML_MPI__BLACS_PROTOTYPES_H
-#define FML_MPI__BLACS_PROTOTYPES_H
+#ifndef FML_MPI_INTERNALS__BLACS_PROTOTYPES_H
+#define FML_MPI_INTERNALS__BLACS_PROTOTYPES_H
 #pragma once
 
 
