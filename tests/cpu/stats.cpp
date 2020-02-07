e1573f6aa0beb70813772c213b126861791857b1
Drew Schmidt <wrathematics@gmail.com>
2020-02-07 06:57:34 -0500

    add pca test

---
+++ tests/cpu/stats.cpp
@@ -0,0 +1,34 @@
+#include "../catch.hpp"
+
+#include <_internals/arraytools/src/arraytools.hpp>
+#include <cpu/stats.hh>
+
+using namespace arraytools;
+
+
+TEMPLATE_TEST_CASE("stats - pca", "[stats]", float, double)
+{
+  len_t m = 3;
+  len_t n = 2;
+  
+  cpumat<TestType> x(m, n);
+  x.fill_linspace(1, m*n);
+  
+  cpuvec<TestType> sdev;
+  cpumat<TestType> rot;
+  stats::pca(true, true, x, sdev, rot);
+  
+  TestType sq2 = sqrt(2.0);
+  
+  REQUIRE( sdev.size() == 2 );
+  REQUIRE( fltcmp::eq(sdev.get(0), sq2) );
+  REQUIRE( fltcmp::eq(sdev.get(1), 0) );
+  
+  REQUIRE( rot.nrows() == 2 );
+  REQUIRE( rot.ncols() == 2 );
+  for (len_t i=0; i<n*n; i++)
+  {
+    auto test = fabs(rot.get(i)); // sign choice is up to LAPACK library
+    REQUIRE( fltcmp::eq(test, 1/sq2) );
+  }
+}
