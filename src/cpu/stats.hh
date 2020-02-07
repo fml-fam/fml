468e61febfd859295aae91803e4ff9e4596388f9
Drew Schmidt <wrathematics@gmail.com>
2020-02-07 06:57:21 -0500

    fix sdev calculation in pca

--- src/cpu/stats.hh
+++ src/cpu/stats.hh
@@ -21,6 +21,14 @@ namespace stats
     cpumat<REAL> u;
     cpumat<REAL> trot;
     linalg::svd(x, sdev, u, trot);
+    
+    const REAL d = 1.0 / sqrt((REAL)std::max(x.nrows()-1, 1));
+    REAL *sdev_d = sdev.data_ptr();
+    
+    #pragma omp for simd
+    for (len_t i=0; i<sdev.size(); i++)
+      sdev_d[i] *= d;
+    
     linalg::xpose(trot, rot);
   }
 }
