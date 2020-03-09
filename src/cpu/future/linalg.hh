edd1a1c9071b929dd9a841bc94407a2956764f5a
Drew Schmidt <wrathematics@gmail.com>
2020-03-08 20:35:46 -0400

    add randomized svd to future

--- src/cpu/future/linalg.hh
+++ src/cpu/future/linalg.hh
@@ -66,6 +66,54 @@ namespace linalg
     sign = sgn;
   }
   
+  
+  
+  template <typename REAL>
+  void rsvd(const int k, const int q, cpumat<REAL> &x, cpuvec<REAL> &s, cpumat<REAL> &u, cpumat<REAL> &vt)
+  {
+    const len_t m = x.nrows();
+    const len_t n = x.ncols();
+    
+    cpumat<REAL> omega(n, 2*k);
+    omega.fill_runif();
+    
+    cpumat<REAL> Y(m, 2*k);
+    cpumat<REAL> QY(m, 2*k);
+    cpumat<REAL> Z(n, 2*k);
+    cpumat<REAL> QZ(n, 2*k);
+    
+    cpuvec<REAL> qraux;
+    cpuvec<REAL> work;
+    
+    cpumat<REAL> B(2*k, n);
+    
+    // Stage A
+    matmult(false, false, (REAL)1.0, x, omega, Y);
+    qr_internals(false, Y, qraux, work);
+    qr_Q(Y, qraux, QY, work);
+    
+    for (int i=1; i<q; i++)
+    {
+      matmult(true, false, (REAL)1.0, x, QY, Z);
+      qr_internals(false, Z, qraux, work);
+      qr_Q(Z, qraux, QZ, work);
+      
+      matmult(false, false, (REAL)1.0, x, QZ, Y);
+      qr_internals(false, Y, qraux, work);
+      qr_Q(Y, qraux, QY, work);
+    }
+    
+    // Stage B
+    matmult(true, false, (REAL)1.0, QY, x, B);
+    
+    cpumat<REAL> uB;
+    svd(B, s, uB, vt);
+    
+    s.resize(k);
+    
+    matmult(false, false, (REAL)1.0, QY, uB, u);
+    u.resize(u.nrows(), k);
+  }
 }
 
 
