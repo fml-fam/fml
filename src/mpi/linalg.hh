a06f7412c82d2288865d7b94faddb85558f262c7
Drew Schmidt <wrathematics@gmail.com>
2020-03-22 13:27:42 -0400

    add tssvd for mpimat

--- src/mpi/linalg.hh
+++ src/mpi/linalg.hh
@@ -15,6 +15,7 @@
 #include "internals/bcutils.hh"
 #include "internals/scalapack.hh"
 #include "mpimat.hh"
+#include "mpihelpers.hh"
 
 
 namespace linalg
@@ -818,6 +819,117 @@ namespace linalg
     R.fill_zero();
     fml::scalapack::lacpy('U', m, n, QR.data_ptr(), QR.desc_ptr(), R.data_ptr(),
       R.desc_ptr());
+  }
+  
+  
+  
+  /**
+    @brief Computes the singular value decomposition for tall/skinny data.
+    The number of rows must be greater than the number of columns. If the number
+    of rows is not significantly larger than the number of columns, this may not
+    be more efficient than simply calling `linalg::svd()`.
+    
+    @details The operation works by computing a QR and then taking the SVD of
+    the R matrix. The left singular vectors are Q times the left singular
+    vectors from R's SVD, and the singular value and the right singular vectors
+    are those from R's SVD.
+    
+    @param[inout] x Input data matrix. Values are overwritten.
+    @param[out] s Vector of singular values.
+    @param[out] u Matrix of left singular vectors.
+    @param[out] vt Matrix of (transposed) right singular vectors.
+    
+    @impl Uses `linalg::qr()` and `linalg::svd()`, and if computing the
+    left/right singular vectors, `linalg::qr_R()` and `linalg::qr_Q()`.
+    
+    @allocs If the any outputs are inappropriately sized, they will
+    automatically be re-allocated. Additionally, some temporary work storage
+    is needed.
+    
+    @except If a (re-)allocation is triggered and fails, a `bad_alloc`
+    exception will be thrown.
+    
+    @tparam REAL should be 'float' or 'double'.
+   */
+  template <typename REAL>
+  void tssvd(mpimat<REAL> &x, cpuvec<REAL> &s, mpimat<REAL> &u, mpimat<REAL> &vt)
+  {
+    check_grid(x, u);
+    check_grid(x, vt);
+    
+    const len_t m = x.nrows();
+    const len_t n = x.ncols();
+    if (m <= n)
+      throw std::runtime_error("'x' must have more rows than cols");
+    
+    grid g = x.get_grid();
+    
+    cpuvec<REAL> qraux(n);
+    cpuvec<REAL> work(m);
+    qr_internals(false, x, qraux, work);
+    
+    mpimat<REAL> R(g, n, n, x.bf_rows(), x.bf_cols());
+    qr_R(x, R);
+    
+    mpimat<REAL> u_R(g, n, n, x.bf_rows(), x.bf_cols());
+    svd(R, s, u_R, vt);
+    
+    u.resize(m, n);
+    u.fill_eye();
+    
+    qr_Q(x, qraux, u, work);
+    
+    matmult(false, false, (REAL)1.0, u, u_R, x);
+    mpihelpers::mpi2mpi(x, u);
+  }
+  
+  /// \overload
+  template <typename REAL>
+  void tssvd(mpimat<REAL> &x, cpuvec<REAL> &s)
+  {
+    const len_t m = x.nrows();
+    const len_t n = x.ncols();
+    if (m <= n)
+      throw std::runtime_error("'x' must have more rows than cols");
+    
+    grid g = x.get_grid();
+    s.resize(n);
+    
+    cpuvec<REAL> qraux(n);
+    cpuvec<REAL> work(m);
+    qr_internals(false, x, qraux, work);
+    
+    const len_t m_local = x.nrows_local();
+    const len_t n_local = x.ncols_local();
+    const len_t mb = x.bf_rows();
+    const len_t nb = x.bf_cols();
+    REAL *x_d = x.data_ptr();
+    
+    for (len_t j=0; j<n_local; j++)
+    {
+      #pragma omp for simd
+      for (len_t i=0; i<n_local; i++)
+      {
+        const int gi = fml::bcutils::l2g(i, mb, g.nprow(), g.myrow());
+        const int gj = fml::bcutils::l2g(j, nb, g.npcol(), g.mycol());
+        
+        if (gi > gj)
+          x_d[i + m_local*j] = (REAL) 0.0;
+      }
+    }
+    
+    int info = 0;
+    
+    REAL tmp;
+    fml::scalapack::gesvd('N', 'N', n, n, x_d, x.desc_ptr(), s.data_ptr(), NULL,
+      NULL, NULL, NULL, &tmp, -1, &info);
+    int lwork = (int) tmp;
+    if (lwork > work.size())
+      work.resize(lwork);
+    
+    fml::scalapack::gesvd('N', 'N', n, n, x_d, x.desc_ptr(), s.data_ptr(), NULL,
+      NULL, NULL, NULL, work.data_ptr(), lwork, &info);
+    fml::linalgutils::check_info(info, "gesvd");
   }
 }
 
