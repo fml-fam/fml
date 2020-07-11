9d15763dde800c9085f2fe92c585e545915f5eb8
Drew Schmidt <wrathematics@gmail.com>
2020-07-11 11:49:17 -0400

    add cpsvd for parmat_cpu

--- src/fml/par/cpu/linalg.hh
+++ src/fml/par/cpu/linalg.hh
@@ -39,6 +39,38 @@ namespace linalg
     crossprod(alpha, x, ret);
     return ret;
   }
+  
+  
+  
+  template <typename REAL>
+  void cpsvd(const parmat_cpu<REAL> &x, cpuvec<REAL> &s, parmat_cpu<REAL> &u,
+    cpumat<REAL> &vt)
+  {
+    auto cp = crossprod((REAL)1, x);
+    x.get_comm().allreduce(cp.nrows()*cp.ncols(), cp.data_ptr());
+    eigen_sym(cp, s, vt);
+    
+    matmult(false, false, (REAL)1, x.data_obj(), vt, u.data_obj());
+    
+    const REAL s_d = s.data_ptr();
+    const REAL u_d = u.data_obj().data_ptr();
+    const len_t m = u.data_obj().nrows();
+    const len_t n = u.ncols();
+    for (len_t j=0; j<n; j++)
+    {
+      for (len_t i=0; i<m; i++)
+        u_d *= s_d[j];
+    }
+  }
+  
+  /// \overload
+  template <typename REAL>
+  void cpsvd(const parmat_cpu<REAL> &x, cpuvec<REAL> &s)
+  {
+    auto cp = crossprod((REAL)1, x);
+    x.get_comm().allreduce(cp.nrows()*cp.ncols(), cp.data_ptr());
+    eigen_sym(cp, s); 
+  }
 }
 }
 
