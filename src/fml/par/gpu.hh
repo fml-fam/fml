fbf28cd353e014cdf3151b314394c88025e2722d
Drew Schmidt <wrathematics@gmail.com>
2020-07-20 20:41:39 -0400

    add get_device_num()

--- src/fml/par/gpu.hh
+++ src/fml/par/gpu.hh
@@ -11,4 +11,30 @@
 #include "gpu/parmat.hh"
 
 
+namespace fml
+{
+  /**
+    @brief Returns the device number of the GPU to use with the calling MPI
+    process.
+    
+    @details 
+    
+    @param[in] c A communicator object.
+    @return The ordinal device number for the calling MPI process to use in,
+    e.g. `fml::new_card()`.
+    
+    @except If there are more MPI ranks within any node than there are GPUS, the
+    function will throw a 'runtime_error' exception.
+   */
+  inline int get_device_num(const comm &c)
+  {
+    int ngpus = get_device_count();
+    if (c.localsize() > ngpus)
+      throw std::runtime_error("parmat_gpu can not be used with more MPI ranks than GPUs per node");
+    
+    return c.rank() % ngpus;
+  }
+}
+
+
 #endif
