61bb8edb5156d49f0264a463cebdea7d43252b2e
Drew Schmidt <wrathematics@gmail.com>
2020-07-21 21:11:10 -0400

    add basic csv reader to future io

--- src/fml/cpu/future/io.hh
+++ src/fml/cpu/future/io.hh
@@ -13,6 +13,8 @@
 #include "../cpumat.hh"
 
 
+namespace fml
+{
 namespace io
 {
   namespace
@@ -107,6 +109,124 @@ namespace io
     
     fclose(f);
   }
+  
+
+  
+  namespace
+  {
+    static inline len_t wc_l(FILE *fp, char *buf, const int buflen)
+    {
+      len_t nrows = 0;
+      int readlen = buflen;
+      
+      while (readlen == buflen)
+      {
+        readlen = fread(buf, sizeof(*buf), buflen, fp);
+        char *ptr = buf;
+        char *last = buf + readlen;
+        
+        while ((ptr = (char*)memchr(ptr, '\n', last - ptr)))
+        {
+          ptr++;
+          nrows++;
+        }
+      }
+      
+      return nrows;
+    }
+    
+    static inline len_t nc(FILE *fp, const char sep, char *buf, const int buflen)
+    {
+      len_t ncols = 0;
+      char *ret = fgets(buf, buflen*sizeof(*buf), fp);
+      (void)ret;
+      
+      int readlen = strlen(buf);
+      
+      #pragma omp simd reduction(+:ncols)
+      for (int i=0; i<readlen; i++)
+        ncols += (buf[i]==sep ? 1 : 0);
+      
+      return ncols+1;
+    }
+    
+    template <typename REAL>
+    static inline void read(FILE *fp, char *buf, const int buflen, cpumat<REAL> &x)
+    {
+      const len_t m = x.nrows();
+      const len_t n = x.ncols();
+      REAL *x_d = x.data_ptr();
+      
+      len_t i = 0;
+      while (fgets(buf, buflen*sizeof(*buf), fp) != NULL)
+      {
+        if (i == m)
+          break;
+        
+        char *ptr = buf;
+        char *last;
+        
+        for (len_t j=0; j<n; j++)
+        {
+          double tmp = strtod(ptr, &last);
+          x_d[i + m*j] = (REAL) tmp;
+          
+          ptr = last+1;
+        }
+        
+        i++;
+      }
+    }
+  }
+  
+  template <typename REAL>
+  void read_csv(const char *filename, cpumat<REAL> &x, const char sep=',')
+  {
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+      throw std::runtime_error("Could not open file");
+    
+    const int buflen = 1024;
+    char *buf = (char*) malloc(buflen * sizeof(*buf));
+    if (buf == NULL)
+      throw std::runtime_error("OOM");
+    
+    len_t m = wc_l(fp, buf, buflen);
+    rewind(fp);
+    len_t n = nc(fp, sep, buf, buflen);
+    rewind(fp);
+    
+    x.resize(m, n);
+    read(fp, buf, buflen, x);
+    
+    fclose(fp);
+    free(buf);
+  }
+  
+  template <typename REAL>
+  void read_csv(const char *filename, cpuvec<REAL> &x)
+  {
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+      throw std::runtime_error("Could not open file");
+    
+    const int buflen = 1024;
+    char *buf = (char*) malloc(buflen * sizeof(*buf));
+    if (buf == NULL)
+      throw std::runtime_error("OOM");
+    
+    len_t m = wc_l(fp, buf, buflen);
+    rewind(fp);
+    len_t n = 1;
+    
+    x.resize(m);
+    cpumat<REAL> x_mat(x.data_ptr(), m, n, false);
+    read(fp, buf, buflen, x_mat);
+    
+    fclose(fp);
+    free(buf);
+  }
+}
 }
 
 
