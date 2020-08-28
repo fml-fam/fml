89c176f3d6c28bdf121adde7640efac965f6c68c
Drew Schmidt <wrathematics@gmail.com>
2020-08-27 20:58:46 -0400

    add restrict header

---
+++ src/fml/_internals/restrict.hh
@@ -0,0 +1,14 @@
+// This file is part of fml which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef FML__INTERNALS_RESTRICT_H
+#define FML__INTERNALS_RESTRICT_H
+
+
+#ifndef restrict
+#define restrict __restrict__
+#endif
+
+
+#endif
