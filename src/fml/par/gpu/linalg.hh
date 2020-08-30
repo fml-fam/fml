11402677fc775da35b13abdbe01337d94117c4d2
Drew Schmidt <wrathematics@gmail.com>
2020-08-30 19:29:58 -0400

    update parmat_gpu linalg.hh

--- src/fml/par/gpu/linalg.hh
+++ src/fml/par/gpu/linalg.hh
@@ -7,42 +7,9 @@
 #pragma once
 
 
-#include "parmat.hh"
-#include "../../gpu/linalg.hh"
-
-
-namespace fml
-{
-namespace linalg
-{
-  /// @brief Computes lower triangle of alpha*x^T*x
-  template <typename REAL>
-  void crossprod(const REAL alpha, const parmat_gpu<REAL> &x, gpumat<REAL> &ret)
-  {
-    err::check_card(x, ret);
-    
-    const len_t n = x.ncols();
-    if (n != ret.nrows() || n != ret.ncols())
-      ret.resize(n, n);
-    
-    linalg::crossprod(alpha, x.data_obj(), ret);
-    
-    comm r = x.get_comm();
-    r.allreduce(n*n, ret.data_ptr());
-  }
-  
-  /// \overload
-  template <typename REAL>
-  gpumat<REAL> crossprod(const REAL alpha, const parmat_gpu<REAL> &x)
-  {
-    const len_t n = x.ncols();
-    gpumat<REAL> ret(x.get_card(), n, n);
-    
-    crossprod(alpha, x, ret);
-    return ret;
-  }
-}
-}
+#include "linalg/blas.hh"
+#include "linalg/qr.hh"
+#include "linalg/svd.hh"
 
 
 #endif
