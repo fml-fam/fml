e9b48435651214713faeec75097cb706771770e9
Drew Schmidt <wrathematics@gmail.com>
2021-08-06 20:55:32 -0400

    add gpu tests for rowsweep and colsweep

--- tests/gpu/dimops.cu
+++ tests/gpu/dimops.cu
@@ -54,6 +54,63 @@ TEMPLATE_TEST_CASE("dimops - means", "[dimops]", float, double)
 
 
 
+
+TEMPLATE_TEST_CASE("dimops - rowsweep", "[dimops]", float, double)
+{
+  len_t m = 3;
+  len_t n = 2;
+  
+  fml::gpumat<TestType> x(c, m, n);
+  fml::gpuvec<TestType> s(c, m);
+  
+  x.fill_linspace(1, m*n);
+  s.fill_linspace(1, m);
+  fml::dimops::rowsweep(x, s, fml::dimops::SWEEP_ADD);
+  
+  REQUIRE( fltcmp::eq(x.get(0, 0), 2) );
+  REQUIRE( fltcmp::eq(x.get(0, 1), 5) );
+  REQUIRE( fltcmp::eq(x.get(2, 0), 6) );
+  REQUIRE( fltcmp::eq(x.get(2, 1), 9) );
+  
+  x.fill_linspace(1, m*n);
+  fml::dimops::rowsweep(x, s, fml::dimops::SWEEP_MUL);
+  
+  REQUIRE( fltcmp::eq(x.get(0, 0), 1) );
+  REQUIRE( fltcmp::eq(x.get(0, 1), 4) );
+  REQUIRE( fltcmp::eq(x.get(2, 0), 9) );
+  REQUIRE( fltcmp::eq(x.get(2, 1), 18) );
+}
+
+
+
+TEMPLATE_TEST_CASE("dimops - colsweep", "[dimops]", float, double)
+{
+  len_t m = 3;
+  len_t n = 2;
+  
+  fml::gpumat<TestType> x(c, m, n);
+  fml::gpuvec<TestType> s(c, n);
+  
+  x.fill_linspace(1, m*n);
+  s.fill_linspace(1, n);
+  fml::dimops::colsweep(x, s, fml::dimops::SWEEP_ADD);
+  
+  REQUIRE( fltcmp::eq(x.get(0, 0), 2) );
+  REQUIRE( fltcmp::eq(x.get(0, 1), 6) );
+  REQUIRE( fltcmp::eq(x.get(2, 0), 4) );
+  REQUIRE( fltcmp::eq(x.get(2, 1), 8) );
+  
+  x.fill_linspace(1, m*n);
+  fml::dimops::colsweep(x, s, fml::dimops::SWEEP_MUL);
+  
+  REQUIRE( fltcmp::eq(x.get(0, 0), 1) );
+  REQUIRE( fltcmp::eq(x.get(0, 1), 8) );
+  REQUIRE( fltcmp::eq(x.get(2, 0), 3) );
+  REQUIRE( fltcmp::eq(x.get(2, 1), 12) );
+}
+
+
+
 TEMPLATE_TEST_CASE("dimops - scale", "[dimops]", float, double)
 {
   len_t m = 3;
