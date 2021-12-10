446bda78593f4a1b28ab7d5f0f72140e4bb79bc4
Drew Schmidt <wrathematics@gmail.com>
2021-12-10 07:47:10 -0500

    add cov methods for mpi

---
+++ src/fml/mpi/stats/cov.hh
@@ -0,0 +1,67 @@
+// This file is part of fml which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef FML_MPI_STATS_COV_H
+#define FML_MPI_STATS_COV_H
+#pragma once
+
+
+#include <stdexcept>
+
+#include "../mpimat.hh"
+
+#include "../dimops.hh"
+
+#include "../linalg/linalg_blas.hh"
+#include "../linalg/linalg_invert.hh"
+
+
+namespace fml
+{
+namespace stats
+{
+  /**
+    @brief Covariance.
+    
+    @details Computes the lower triangle of the variance-covariance matrix.
+    Centering is done in-place.
+    
+    @param[inout] x,y Input data. For the matrix variants, data is mean-centered
+    on return.
+    @param[out] cov The covariance matrix.
+    
+    @except In the matrix-matrix and vector-vector variants, if the object
+    dimensions/sizes are non-conformable, a `runtime_error` exception is thrown.
+    
+    @tparam REAL should be 'float' or 'double'.
+  */
+  template <typename REAL>
+  void cov(mpimat<REAL> &x, mpimat<REAL> &cov)
+  {
+    dimops::scale(true, false, x);
+    
+    const REAL alpha = 1. / ((REAL) (x.nrows()-1));
+    linalg::crossprod(alpha, x, cov);
+  }
+  
+  
+  
+  /// \overload
+  template <typename REAL>
+  void cov(mpimat<REAL> &x, mpimat<REAL> &y, mpimat<REAL> &cov)
+  {
+    if (x.nrows() != y.nrows())
+      throw std::runtime_error("non-conformable arguments");
+    
+    dimops::scale(true, false, x);
+    dimops::scale(true, false, y);
+    
+    const REAL alpha = 1. / ((REAL) (x.nrows()-1));
+    linalg::matmult(true, false, alpha, x, y, cov);
+  }
+}
+}
+
+
+#endif
--- tests/mpi/stats.cpp
+++ tests/mpi/stats.cpp
@@ -34,3 +34,25 @@ TEMPLATE_TEST_CASE("stats - pca", "[stats]", float, double)
     REQUIRE( fltcmp::eq(test, 1/sq2) );
   }
 }
+
+
+
+TEMPLATE_TEST_CASE("stats - cov", "[stats]", float, double)
+{
+  len_t m = 3;
+  len_t n = 2;
+  
+  fml::mpimat<TestType> x(g, m, n, 1, 1);
+  x.fill_linspace();
+  x.set(3, 2);
+  x.set(4, 0);
+  x.set(5, 1);
+  
+  fml::mpimat<TestType> cov(g, 1, 1);
+  
+  // mat
+  fml::stats::cov(x, cov);
+  REQUIRE( fltcmp::eq(cov.get(0), 1) );
+  REQUIRE( fltcmp::eq(cov.get(1), -.5) );
+  REQUIRE( fltcmp::eq(cov.get(3), 1) );
+}
