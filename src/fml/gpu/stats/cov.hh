c5ddda6464d5bf365e681df5b3151ec4cc464aae
Drew Schmidt <wrathematics@gmail.com>
2021-12-11 12:40:25 -0500

    add cov methods for gpu

---
+++ src/fml/gpu/stats/cov.hh
@@ -0,0 +1,115 @@
+// This file is part of fml which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef FML_GPU_STATS_COV_H
+#define FML_GPU_STATS_COV_H
+#pragma once
+
+
+#include <stdexcept>
+
+#include "../internals/gpuscalar.hh"
+
+#include "../gpumat.hh"
+#include "../gpuvec.hh"
+
+#include "../dimops.hh"
+
+#include "../linalg/linalg_blas.hh"
+#include "../linalg/linalg_invert.hh"
+
+
+namespace fml
+{
+namespace stats
+{
+  /**
+    @brief Covariance.
+    
+    @details Computes the lower triangle of the variance-covariance matrix.
+    Centering is done in-place.
+    
+    @param[inout] x,y Input data. For the matrix variants, data is mean-centered
+    on return.
+    @param[out] cov The covariance matrix.
+    
+    @except In the matrix-matrix and vector-vector variants, if the object
+    dimensions/sizes are non-conformable, a `runtime_error` exception is thrown.
+    
+    @tparam REAL should be 'float' or 'double'.
+  */
+  template <typename REAL>
+  void cov(gpumat<REAL> &x, gpumat<REAL> &cov)
+  {
+    dimops::scale(true, false, x);
+    
+    const REAL alpha = 1. / ((REAL) (x.nrows()-1));
+    linalg::crossprod(alpha, x, cov);
+  }
+  
+  
+  
+  /// \overload
+  template <typename REAL>
+  void cov(gpumat<REAL> &x, gpumat<REAL> &y, gpumat<REAL> &cov)
+  {
+    if (x.nrows() != y.nrows())
+      throw std::runtime_error("non-conformable arguments");
+    
+    dimops::scale(true, false, x);
+    dimops::scale(true, false, y);
+    
+    const REAL alpha = 1. / ((REAL) (x.nrows()-1));
+    linalg::matmult(true, false, alpha, x, y, cov);
+  }
+  
+  
+  
+  namespace internals
+  {
+    template <typename REAL>
+    __global__ void kernel_cov_vecvec(const len_t n, const REAL *x,
+    const REAL *y, REAL *sxy, REAL *sx, REAL *sy)
+    {
+      int i = blockDim.x*blockIdx.x + threadIdx.x;
+      int j = blockDim.y*blockIdx.y + threadIdx.y;
+      
+      if (i < n && j < n)
+      {
+        atomicAdd(sxy, x[i]*y[i]);
+        atomicAdd(sx, x[i]);
+        atomicAdd(sy, y[i]);
+      }
+    }
+  }
+  
+  /// \overload
+  template <typename REAL>
+  REAL cov(const gpuvec<REAL> &x, const gpuvec<REAL> &y)
+  {
+    const len_t n = x.size();
+    if (n != y.size())
+      throw std::runtime_error("non-conformal arguments");
+    
+    REAL sxy, sx, sy;
+    sxy = sx = sy = 0;
+    gpuscalar<REAL> sxy_gpu(x.get_card(), sxy);
+    gpuscalar<REAL> sx_gpu(x.get_card(), sx);
+    gpuscalar<REAL> sy_gpu(x.get_card(), sy);
+    
+    internals::kernel_cov_vecvec<<<x.get_griddim(), x.get_blockdim()>>>(n,
+      x.data_ptr(), y.data_ptr(), sxy_gpu.data_ptr(), sx_gpu.data_ptr(),
+      sy_gpu.data_ptr());
+    
+    sxy_gpu.get_val(&sxy);
+    sx_gpu.get_val(&sx);
+    sy_gpu.get_val(&sy);
+    
+    return (sxy - (sx*sy*((REAL) 1./n))) * ((REAL) 1./(n-1));
+  }
+}
+}
+
+
+#endif
--- tests/gpu/stats.cu
+++ tests/gpu/stats.cu
@@ -37,3 +37,37 @@ TEMPLATE_TEST_CASE("stats - pca", "[stats]", float, double)
     REQUIRE( fltcmp::eq(test, 1/sq2) );
   }
 }
+
+
+
+TEMPLATE_TEST_CASE("stats - cov", "[stats]", float, double)
+{
+  len_t m = 3;
+  len_t n = 2;
+  
+  fml::gpumat<TestType> x(c, m, n);
+  x.fill_linspace();
+  x.set(3, 2);
+  x.set(4, 0);
+  x.set(5, 1);
+  
+  fml::gpumat<TestType> cov(c);
+  
+  // mat
+  fml::stats::cov(x, cov);
+  REQUIRE( fltcmp::eq(cov.get(0), 1) );
+  REQUIRE( fltcmp::eq(cov.get(1), -.5) );
+  REQUIRE( fltcmp::eq(cov.get(3), 1) );
+  
+  // vec-vec
+  fml::gpuvec<TestType> x1(c);
+  fml::gpuvec<TestType> x2(c);
+  x.get_col(0, x1);
+  x.get_col(1, x2);
+  
+  TestType cv;
+  cv = fml::stats::cov(x1, x2);
+  REQUIRE( fltcmp::eq(cv, -.5) );
+  cv = fml::stats::cov(x2, x2);
+  REQUIRE( fltcmp::eq(cv, 1) );
+}
