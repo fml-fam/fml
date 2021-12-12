4cf0a2ae1e0eb7a709b4642e750190114734d34b
Drew Schmidt <wrathematics@gmail.com>
2021-12-12 15:17:12 -0500

    better cov tests, add cor tests

--- tests/cpu/stats.cpp
+++ tests/cpu/stats.cpp
@@ -46,17 +46,22 @@ TEMPLATE_TEST_CASE("stats - cov", "[stats]", float, double)
   x.fill_linspace();
   x.set(3, 2);
   x.set(4, 0);
-  x.set(5, 1);
+  x.set(5, -1);
   
   fml::cpumat<TestType> cov;
   
   // mat
   fml::stats::cov(x, cov);
   REQUIRE( fltcmp::eq(cov.get(0), 1) );
-  REQUIRE( fltcmp::eq(cov.get(1), -.5) );
-  REQUIRE( fltcmp::eq(cov.get(3), 1) );
+  REQUIRE( fltcmp::eq(cov.get(1), -1.5) );
+  REQUIRE( fltcmp::eq(cov.get(3), 2+1.0/3.0) );
   
   // vec-vec
+  x.fill_linspace();
+  x.set(3, 2);
+  x.set(4, 0);
+  x.set(5, -1);
+  
   fml::cpuvec<TestType> x1;
   fml::cpuvec<TestType> x2;
   x.get_col(0, x1);
@@ -64,7 +69,46 @@ TEMPLATE_TEST_CASE("stats - cov", "[stats]", float, double)
   
   TestType cv;
   cv = fml::stats::cov(x1, x2);
-  REQUIRE( fltcmp::eq(cv, -.5) );
+  REQUIRE( fltcmp::eq(cv, -1.5) );
   cv = fml::stats::cov(x2, x2);
-  REQUIRE( fltcmp::eq(cv, 1) );
+  REQUIRE( fltcmp::eq(cv, 2+1.0/3.0) );
+}
+
+
+
+TEMPLATE_TEST_CASE("stats - cor", "[stats]", float, double)
+{
+  len_t m = 3;
+  len_t n = 2;
+  
+  fml::cpumat<TestType> x(m, n);
+  x.fill_linspace();
+  x.set(3, 2);
+  x.set(4, 0);
+  x.set(5, -1);
+  
+  fml::cpumat<TestType> cor;
+  
+  // mat
+  fml::stats::cor(x, cor);
+  REQUIRE( fltcmp::eq(cor.get(0), 1) );
+  REQUIRE( fltcmp::eq(cor.get(1), -3.0/sqrt(2.0*14.0/3.0)) );
+  REQUIRE( fltcmp::eq(cor.get(3), 1) );
+  
+  // vec-vec
+  x.fill_linspace();
+  x.set(3, 2);
+  x.set(4, 0);
+  x.set(5, -1);
+  
+  fml::cpuvec<TestType> x1;
+  fml::cpuvec<TestType> x2;
+  x.get_col(0, x1);
+  x.get_col(1, x2);
+  
+  TestType cr;
+  cr = fml::stats::cor(x1, x2);
+  REQUIRE( fltcmp::eq(cr, -3.0/sqrt(2.0*14.0/3.0)) );
+  cr = fml::stats::cor(x2, x2);
+  REQUIRE( fltcmp::eq(cr, 1) );
 }
